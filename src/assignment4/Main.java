12e3d7ee13e6714b28cf0e5706ce957080eac4d3
Hackerman64 <jonahlharris@utexas.edu>
2016-10-19 16:25:40 -0600

    moved

--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -122,11 +122,13 @@ public class Main {
         		}
         		
         	} else if (splitUserInput[splitIndex].equals("stats")) {		
-        		userInput = kb.next();
+        		//userInput = kb.next();
+        		splitIndex += 1;
+        		String statClass = splitUserInput[splitIndex];
         		try {
-        			Class<?> className = Class.forName(myPackage + "." + userInput);		// Get class of input
+        			Class<?> className = Class.forName(myPackage + "." + statClass);		// Get class of input
         			Method runStatsMethod = className.getMethod("runStats", new Class<?>[]{List.class});
-        			runStatsMethod.invoke(null, Critter.getInstances(userInput));	// Input null because static method, second parameter is the list
+        			runStatsMethod.invoke(null, Critter.getInstances(statClass));	// Input null because static method, second parameter is the list
           		} catch (Exception e){
         			System.out.println(e.toString());
         			return;
