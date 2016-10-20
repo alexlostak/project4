e9d6873d8cf8764eb4164c08418fc8712be239d6
Alex Lostak <alex@cubeatx.com>
2016-10-19 22:59:55 -0500

    Team Plan PDF

--- src/assignment4/Lion.java
+++ src/assignment4/Lion.java
@@ -5,7 +5,11 @@ import assignment4.Critter;
 public class Lion  extends Critter {
 	public String toString() { return "L"; }
 	
-	public boolean fight(String not_used) { return true; }
+	public boolean fight(String not_used) { 
+		
+		return true; 
+		
+	}
 	
 	public void doTimeStep() {
 		
