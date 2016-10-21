fc2786f47b306f848df22ffd5b488833bd799a94
Hackerman64 <jonahlharris@utexas.edu>
2016-10-20 19:14:31 -0600

    Jonah's Critters! YAYAYAYAYAYAYAYAYAY!!!
    BUNNIES AND CHEETAHS!!!!

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,26 +1,58 @@
+/*
+ * Jonah Harris - Critter 1 of 2
+ * 
+ * Critter1 acts as a bunny; that is, it only fights algae critters for food and flees from other critters
+ * in fights. It reproduces at the minimum provided energy rate! (WOW) and continues in a single 
+ * direction to find algae. Stats displays the total number of Critter1's, the total offspring born, and 
+ * the total amount of algae eaten.
+ */
+
 package assignment4;
 
 public class Critter1 extends Critter {
 	
+	int dir;
+	int baby_bunnies;
+	int algae_eaten;
+	
 	@Override
 	public String toString() { return "1"; }
 	
-	
-	
 	public Critter1() {
-		
+		dir = Critter.getRandomInt(8);
+		baby_bunnies = 0;
+		algae_eaten = 0;
 	}
 	
 	public void doTimeStep() {
-		
+		run(dir);
+		if (getEnergy() >= Params.min_reproduce_energy){
+			Critter2 child = new Critter2();
+			reproduce(child, getRandomInt(8));
+			baby_bunnies += 1;
+		}
 	}
 
 
 
 	@Override
 	public boolean fight(String opponent) { 
-		if (opponent.equals("Algae")) { return true; }
+		if (opponent.equals("@")) { algae_eaten += 1; return true; }
 		else { return false; }
+	}
+	
+	public static void runStats(java.util.List<Critter> bunnies) {
+		int total_baby_bunnies = 0;
+		int total_algae_eaten = 0;
+		for (Object obj : bunnies){
+			Critter1 b = (Critter1) obj;
+			total_baby_bunnies = b.baby_bunnies;	
+			total_algae_eaten = b.algae_eaten;
+		}
+		System.out.print("" + bunnies.size() + " total Critter1    ");
+		System.out.print("" + total_baby_bunnies + " Total Baby Critter1's   ");
+		System.out.print("" + total_algae_eaten + " Total Algae Eaten   ");
+		System.out.println("OM NOM NOM!!!  ^^^ ");
 	}
 
 }
--- src/assignment4/Critter2.java
+++ src/assignment4/Critter2.java
@@ -1,28 +1,59 @@
+/*
+ * Jonah Harris - Critter 2 of 2
+ * 
+ * Critter1 acts as a cheetah; that is, it fights any critters for food. It reproduces at
+ * energy level 200 as it needs a lot of energy to keep running. This critter runs in a 
+ * straight line either left or right, looking for prey. When the stats method is called, 
+ * data is output showing the total cheetahs going left, right, and how many laps all the 
+ * combined cheetahs have ran based on the world width. 
+ */
 package assignment4;
 
 public class Critter2 extends Critter {
+	 
+	int dir;
+	int run_calls;
 	
 	@Override
 	public String toString() { return "2"; }
 	
-	
-	
 	public Critter2() {
-		
+		dir = getRandomInt(2);
+		if (dir == 0) { dir = 0; }
+		else if (dir == 1){ dir = 4; }
+		run_calls = 0;
 	}
 	
 	@Override
 	public void doTimeStep() {
-		walk(6);
-		walk(6);
+		run(dir);
+		run_calls += 1;
+		if (getEnergy() > 200){
+			Critter2 child = new Critter2();
+			reproduce(child, getRandomInt(8));
+		}
 	}
-
-
 
 	@Override
-	public boolean fight(String not_used) { 
-		run(2);
-		return true; 
-	}
+	public boolean fight(String not_used) {	return true; }
 	
+	public static void runStats(java.util.List<Critter> cheetahs) {
+		int total_left = 0;
+		int total_right = 0;
+		int total_runs = 0;
+		int total_laps = 0;
+		int runs_per_lap = Params.world_width / 2;
+		for (Object obj : cheetahs){
+			Critter2 c = (Critter2) obj;
+			if (c.dir == 0) { total_right += 1; }
+			if (c.dir == 4) { total_left  += 1; }
+			total_runs += c.run_calls;
+		}
+		total_laps = total_runs / runs_per_lap;
+		System.out.print("" + cheetahs.size() + " total Critter2    ");
+		System.out.print("" + total_right + " Total Right   ");
+		System.out.print("" + total_left + " Total Left   ");
+		System.out.print("" + total_laps + " Total Laps Ran   ");
+		System.out.println("RAWWWWWR!!!   ");
+	}
 }
